0faef59bc19f8f2156993ffee33366a492fce5ff
Kushagraved <kushagraved@gmail.com>
2023-02-24 11:14:25 +0530

    chore: initialize repository

---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PageNotFound.css'
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <div className='pageNotFoundContainer'>
+        <p>404 Error. Page not found</p>
+      </div>
+    </div>
+  )
+}
+
+export default PageNotFound
